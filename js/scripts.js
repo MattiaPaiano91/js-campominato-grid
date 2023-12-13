0980e0d8ec74610f7453649cc1d383b4aac2c57a
MattiaPaiano91 <paianomattia1@gmail.com>
2023-12-13 18:44:16 +0100

    esercizio completato

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+
+// Consegna
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un 
+// messaggio in console con il numero della cella cliccata.
+
+// Consigli del giorno:
+// Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
+// Ad esempio:
+// Di cosa ho bisogno per generare i numeri?
+// Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
+// Le validazioni e i controlli possiamo farli anche in un secondo momento.
+
+let button = document.getElementById('button');
+// let box = ""
+let container = document.getElementById("grid-box");
+
+button.addEventListener('click', function() {
+    
+  for (let i = 0; i <= 99; i++) {
+        // let box = `<div class="box">${[i + 1]}</div>`;
+        let box = document.createElement("div");
+        box.classList.add("box");
+        container.append(box);
+        box.innerHTML = [i + 1]
+        
+        box.addEventListener("click", function () {
+          box.classList.toggle("clicked");
+          console.log('Complimenti! hai cliccato la cella numero: ' + [i + 1])
+        });  
+      }
+
+});
+
+
+
+
+
+
+
+        
+
+
+        
+    
+
